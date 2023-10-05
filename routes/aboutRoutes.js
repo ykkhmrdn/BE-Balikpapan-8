8ecc365444f5fac313c6bea5a858f1653edd24ea
Pradadipa <putu.pradadipa@gmail.com>
2023-10-05 20:12:16 +0800

    update-route

--- routes/aboutRoutes.js
+++ routes/aboutRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const aboutRoute = express.Router();
 const aboutController = require('../controllers/aboutController')
 
-aboutRoute.get('/', aboutController.getAllAbout)
+aboutRoute.get('/views/about', aboutController.getAllAbout)
 
 
 module.exports = { aboutRoute }
