c7d4e361008f77633609009e10221a4ed0e72bd4
Pradadipa <putu.pradadipa@gmail.com>
2023-10-06 02:40:57 +0800

    update route

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,7 +4,7 @@ const productController = require('../controllers/productsController')
 
 productRoute.get('/index', productController.getAllProduct)
 productRoute.get('/views/products', productController.getAllProduct)
-productRoute.get('/views/detail-products/:byId', productController.getProductById)
+productRoute.get('/views/detail-products.html', productController.getProductById)
 
 
 
