83c7433f441dda4a63fd5d2ae6d6e320c96ee783
zyxkoo15 <ykkhmrdn@gmail.com>
2023-10-07 17:22:51 +0700

    fix controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,24 +1,21 @@
-const productService = require('../services/productsService')
+const productService = require('../services/productsService');
 
-//Get
+// Get all products
 const getAllProduct = async (req, res) => {
-   try{
-      const product = await productService.getAllProduct()
-      res.status(200).json({
-      data: product
-   })
-   } catch (error){
-      console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
-   }
-}
+  try {
+    const product = await productService.getAllProduct();
+    res.status(200).json({
+      data: product,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
-
-
-//Get by Id
-const getProductById = async (request, res) => {
-<<<<<<< HEAD
-  const { productId } = request.query;
+// Get product by Id
+const getProductById = async (req, res) => {
+  const { productId } = req.query;
   try {
     const product = await productService.getProductById(productId);
     if (!product) {
@@ -27,31 +24,6 @@ const getProductById = async (request, res) => {
     res.status(200).json({
       message: 'Successfully fetched product',
       data: product,
-=======
-   const { byId } = request.params; // Use 'byId' instead of 'userid'
-   try {
-     const product = await productService.getProductById(byId); // Pass 'byId' as the argument
-     if (!product) {
-       return res.status(404).json({ error: 'Product not found' });
-     }
-     res.status(200).json({
-       message: 'Successfully fetched product',
-       data: product,
-     });
-   } catch (error) {
-     console.error(error);
-     res.status(500).json({ error: 'Internal server error' });
-   }
- };
-// Get products by type
-const getProductsByType = async (req, res) => {
-  const { type } = req.query;
-  try {
-    const products = await productService.getProductsByType(type);
-    res.status(200).json({
-      message: 'Successfully fetched products by type',
-      data: products,
->>>>>>> 5fe1f9446f891a0a4d144dffcd3fd3029d6e37ee
     });
   } catch (error) {
     console.error(error);
@@ -59,36 +31,11 @@ const getProductsByType = async (req, res) => {
   }
 };
 
-<<<<<<< HEAD
-
 // Get products by type
 const getProductsByType = async (req, res) => {
   const { type } = req.query;
   try {
     const products = await productService.getProductsByType(type);
-=======
-// Get products by Name
-const getProductsByName = async (req, res) => {
-  const { name } = req.params;
-  try {
-    const products = await productService.getProductsByName(name);
-    res.status(200).json({
-      message: 'Successfully fetched products by type',
-      data: products,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-};
-
-
-// Get products by type2
-const getProductsByType2 = async (req, res) => {
-  const { type } = req.params;
-  try {
-    const products = await productService.getProductsByType2(type);
->>>>>>> 5fe1f9446f891a0a4d144dffcd3fd3029d6e37ee
     res.status(200).json({
       message: 'Successfully fetched products by type',
       data: products,
@@ -104,8 +51,9 @@ const renderProductsHTML = (req, res) => {
   res.sendFile(path.join(__dirname, '../views/products.html'));
 };
 
-<<<<<<< HEAD
- module.exports = { getAllProduct, getProductById, getProductsByType, renderProductsHTML }
-=======
- module.exports = { getAllProduct, getProductById, getProductsByType, renderProductsHTML, getProductsByName, getProductsByType2}
->>>>>>> 5fe1f9446f891a0a4d144dffcd3fd3029d6e37ee
+module.exports = {
+  getAllProduct,
+  getProductById,
+  getProductsByType,
+  renderProductsHTML,
+};
